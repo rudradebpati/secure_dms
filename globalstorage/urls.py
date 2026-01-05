66966b75cf3cfe1eed88c8a6f545371ae07be0d1
Rudradeb <rudradeb.pati@shyamfuture.com>
2026-01-05 13:09:36 +0530

    URLs has methods commented

--- globalstorage/urls.py
+++ globalstorage/urls.py
@@ -6,20 +6,20 @@ urlpatterns = [
         "folders/<int:dir_id>/",
         views.DirectoryListCreateView.as_view(),
         name="list_directory",
-    ),
+    ), # GET, POST
     path(
         "open_folder/<int:pk>/",
         views.DirectoryDetailView.as_view(),
         name="create_directory",
-    ),
+    ),  # GET
     path(
         "files/",
         views.FileListCreateView.as_view(),
         name="list_files",
-    ),
+    ),  # GET, POST
     path(
-        "files/<int:pk/download/",
+        "files/<int:pk>/download/",
         views.FileDownloadView.as_view(),
         name="file-download",
-    ),
+    ),  # GET
 ]
